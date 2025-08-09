907c233004b6e5a0c8565ccd1770aa1750fdcb68
Rafael Gonzaga <rafaelgonzag4@gmail.com>
2025-08-09 14:37:23 -0300

    commit

--- pinea/app.py
+++ pinea/app.py
@@ -14,7 +14,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
-                   "https://minha-os-app.vercel.app"],
+                   "https://pineaos-rafaelgonzagafrs-projects.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
