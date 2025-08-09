31703644583902e45f7077fe76cac923ba06f225
Rafael Gonzaga <107717855+RafaelGonzagaFr@users.noreply.github.com>
2025-08-09 13:52:27 -0300

    Add files via upload

---
+++ pinea/app.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+from pinea.routers import os
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title="pinea",
+    description="API para geração de OS.",
+    docs_url="/docs",
+    redoc_url=None,
+    openapi_url="/openapi.json",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000",
+                   "https://minha-os-app.vercel.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(os.router)
